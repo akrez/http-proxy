421fa8692e41e2e27076f13ea6f146b9683d544d
Akrez <akrez.like@gmail.com>
2025-10-16 19:24:37 +0330

    up

--- client/addons.py
+++ client/addons.py
@@ -9,11 +9,19 @@ from time import gmtime, strftime
 import configparser
 
 
+config = {
+    "selected_profile_name": None,
+    "local_server_port": 8080,
+    "host_script_url": "",
+    "new_host_header": "",
+    "mode": ""
+}
+
+
 class InlineMode:
     def __init__(self, host_script_url: str, host_header: str = ""):
         self.new_uri = urlparse(host_script_url)
         self.new_host_header = host_header.strip() or self.new_uri.hostname
-
     def request(self, flow: HTTPFlow):
         new_scheme = self.new_uri.scheme
         new_port = (
@@ -32,44 +40,49 @@ class InlineMode:
         #
         # print(f"[{strftime('%H:%M:%S', gmtime())}] {flow.request.method.ljust(8, ' ')}{old_host}")
         # print(assemble_request(flow.request))
-
     def response(self, flow: http.HTTPFlow):
         # print(f"[{strftime('%H:%M:%S', gmtime())}] {flow.response.status_code} {flow.request.scheme}://{flow.request.host}{flow.request.path}\r")
         pass
 
 
-config = configparser.ConfigParser()
-if not config.read("config.ini"):
-    raise FileNotFoundError(
-        "Configuration file 'config.ini' not found or cannot be read"
-    )
+def read_config_ini():
+    config = configparser.ConfigParser()
+    if not config.read("config.ini"):
+        raise FileNotFoundError(
+            "Configuration file 'config.ini' not found or cannot be read"
+        )
+    selected_profile_name = config.get("config", "selected_profile_name", fallback="")
+    if not selected_profile_name in config:
+        available_profiles = ", ".join(f"'{p}'" for p in config.sections())
+        raise ValueError(
+            f"Profile '{selected_profile_name}' not found in 'config.ini'. Available profiles: {available_profiles}"
+        )
+    return {
+        "selected_profile_name": selected_profile_name,
+        "local_server_port": config.get("config", "local_server_port", fallback=""),
+        "host_script_url": config.get(selected_profile_name, "host_script_url", fallback=""),
+        "new_host_header": config.get(selected_profile_name, "new_host_header", fallback=""),
+        "mode": config.get(selected_profile_name, "mode", fallback="")
+    }
 
-local_server_port = int(config.get("config", "local_server_port", fallback=8080))
-selected_profile_name = config.get("config", "selected_profile_name", fallback="")
 
-if not selected_profile_name in config:
-    available_profiles = ", ".join(f"'{p}'" for p in config.sections())
-    raise ValueError(
-        f"Profile '{selected_profile_name}' not found in 'config.ini'. Available profiles: {available_profiles}"
-    )
+if not config["selected_profile_name"]:
+    config = read_config_ini()
 
-if not config.has_option(selected_profile_name, "host_script_url"):
-    raise ValueError(
-        f"Required option 'host_script_url' not found in profile '{selected_profile_name}'"
-    )
-host_script_url = config.get(selected_profile_name, "host_script_url", fallback="")
 
-new_host_header = config.get(selected_profile_name, "new_host_header", fallback="")
+config["local_server_port"] = int(config["local_server_port"] if config["local_server_port"] else 8080)
+if not config["host_script_url"]:
+    raise ValueError(f"host_script_url is required")
+config["mode"] = config["mode"] if config["mode"] in ("inline", "inbody") else "inline"
 
-mode = config.get(selected_profile_name, "mode", fallback="inline")
-if mode not in ("inline", "inbody"):
-    mode = "inline"
 
-print(f"\n[config]\nselected_profile_name={selected_profile_name}\nlocal_server_port={local_server_port}\n\n[{selected_profile_name}]\nhost_script_url={host_script_url}\nnew_host_header={new_host_header}\nmode={mode}\n")
+print(f"\n[config]\nselected_profile_name={config["selected_profile_name"]}\nlocal_server_port={config["local_server_port"]}\n\n[{config["selected_profile_name"]}]\nhost_script_url={config["host_script_url"]}\nnew_host_header={config["new_host_header"]}\nmode={config["mode"]}\n")
 
-ctx.options.listen_port = local_server_port
+
+ctx.options.listen_port = config["local_server_port"]
 ctx.options.connection_strategy = "lazy"
 ctx.options.ssl_insecure = True
 ctx.options.stream_large_bodies = "128k"
 
-addons = [InlineMode(host_script_url, new_host_header)]
+
+addons = [InlineMode(config["host_script_url"], config["new_host_header"])]
